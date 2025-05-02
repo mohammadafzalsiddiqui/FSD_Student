6cd97af20564453d0d4ba42f6f710c20b58c50d3
mohammadafzalsiddiqui <mohammadafzalsiddiqui41@gmail.com>
2025-05-02 15:14:04 +0530

    updated frontend

--- frontend/adi/src/component/Delete.jsx
+++ frontend/adi/src/component/Delete.jsx
@@ -5,7 +5,7 @@ function Delete() {
     const adi=async(e)=>{
         e.preventDefault
         const id=e.target.id.value;
-        await axios.delete(`http://localhost:9000/users/${id}`)
+        await axios.delete(`https://fsd-student.onrender.com/${id}`)
         alert("data delete")
     }
   return (
--- frontend/adi/src/component/Updata.jsx
+++ frontend/adi/src/component/Updata.jsx
@@ -8,7 +8,7 @@ function Updata() {
         const name=e.target.name.value;
         const age =e.target.age.value;
         const data={name,age}
-        await axios.put(`http://localhost:9000/users/${id}`,data)
+        await axios.put(`https://fsd-student.onrender.com/${id}`,data)
         alert("DATA UPDATE")
     }
   return (
